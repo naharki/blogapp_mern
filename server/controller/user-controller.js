5b92308a294f11ebfadaf129c101fadc2ba44db1
naharki <sujannaharki68@gmail.com>
2023-05-02 20:46:57 +0545

    part2 comp but problem in error showning in login page

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,12 +1,16 @@
 import User from '../model/user.js';
+import bcrypt from 'bcrypt';
 
 export const signupUser = async (request, response) => {
   try {
-    const user = request.body;
+    //append random character before hash password as, 
+//    const salt = await bcrypt.genSalt();
+    const hashPassword = await bcrypt.hash(request.body.password, 10);
+    const user = {username: request.body.username, name: request.body.name, password: hashPassword}
     const newUser = new User(user);
     await newUser.save();
-    return response.status(200).json({ msg: 'Signup Successfull' });
+    return response.status(200).json({ msg: "Signup Successfull"});
   } catch (error) {
-    return response.status(500).json({msg: "Error while signup the user"})
+    return response.status(500).json({msg: "Error while signup the user"});
   }
 };
